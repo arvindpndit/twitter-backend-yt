254856bfed54f360108fdcc52a87b3b7491cea8d
arvindpndit <arvindpandit9808@gmail.com>
2024-06-11 15:37:27 +0530

    refactor: api v1 version

---
+++ src/controllers/auth-controller.js
@@ -0,0 +1,88 @@
+const { PrismaClient } = require("@prisma/client");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const prisma = new PrismaClient();
+const JWT_TOKEN_SECRET = process.env.JWT_TOKEN_SECRET;
+
+//app.METHOD(PATH, HANDLER)
+
+const login = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+
+    const user = await prisma.user.findUnique({
+      where: {
+        username: username,
+      },
+    });
+
+    if (!user) {
+      return res.status(401).json({
+        success: false,
+        message: "User doesn't exist, Please Sign up first",
+        data: {},
+        error: {},
+      });
+    }
+
+    const isPasswordCorrect = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordCorrect) {
+      return res.status(401).json({
+        success: false,
+        message: "Incorrect password",
+        data: {},
+        error: {},
+        k,
+      });
+    }
+
+    const token = jwt.sign({ username: user.username }, JWT_TOKEN_SECRET);
+    return res.status(500).json({
+      success: true,
+      message: "Logged in",
+      data: { token },
+      error: {},
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: true,
+      message: "Something went wrong",
+      data: {},
+      error: error,
+    });
+  }
+};
+
+const signup = async (req, res) => {
+  const { username, password, firstName } = req.body;
+  try {
+    const user = await prisma.user.create({
+      data: {
+        username: username,
+        password: await bcrypt.hash(password, 5),
+        firtName: firstName,
+      },
+    });
+
+    return res.status(201).json({
+      success: true,
+      message: "Succesfully created a new user",
+      data: user,
+      error: {},
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: true,
+      message: "Something went wrong",
+      data: {},
+      error: error,
+    });
+  }
+};
+
+module.exports = {
+  login,
+  signup,
+};
--- src/index.js
+++ src/index.js
@@ -1,91 +1,11 @@
 const express = require("express");
-const { PrismaClient } = require("@prisma/client");
-const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
+const apiRoutes = require("./routes/index.js");
 
 const app = express();
 app.use(express.json());
 const PORT = 3000;
 
-const prisma = new PrismaClient();
-const JWT_TOKEN_SECRET = process.env.JWT_TOKEN_SECRET;
-
-//app.METHOD(PATH, HANDLER)
-
-app.post("/login", async (req, res) => {
-  try {
-    const { username, password } = req.body;
-
-    const user = await prisma.user.findUnique({
-      where: {
-        username: username,
-      },
-    });
-
-    if (!user) {
-      return res.status(401).json({
-        success: false,
-        message: "User doesn't exist, Please Sign up first",
-        data: {},
-        error: {},
-      });
-    }
-
-    const isPasswordCorrect = await bcrypt.compare(password, user.password);
-
-    if (!isPasswordCorrect) {
-      return res.status(401).json({
-        success: false,
-        message: "Incorrect password",
-        data: {},
-        error: {},
-        k,
-      });
-    }
-
-    const token = jwt.sign({ username: user.username }, JWT_TOKEN_SECRET);
-    return res.status(500).json({
-      success: true,
-      message: "Logged in",
-      data: { token },
-      error: {},
-    });
-  } catch (error) {
-    return res.status(500).json({
-      success: true,
-      message: "Something went wrong",
-      data: {},
-      error: error,
-    });
-  }
-});
-
-app.post("/signup", async (req, res) => {
-  const { username, password, firstName } = req.body;
-  try {
-    const user = await prisma.user.create({
-      data: {
-        username: username,
-        password: await bcrypt.hash(password, 5),
-        firtName: firstName,
-      },
-    });
-
-    return res.status(201).json({
-      success: true,
-      message: "Succesfully created a new user",
-      data: user,
-      error: {},
-    });
-  } catch (error) {
-    return res.status(500).json({
-      success: true,
-      message: "Something went wrong",
-      data: {},
-      error: error,
-    });
-  }
-});
+app.use("/api", apiRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server started at port ${PORT}`);
